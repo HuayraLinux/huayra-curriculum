b59d95617e80e2e2d3b0e3656b1a888cdec28b2e
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-12-31 11:44:40 -0300

    release 0.0.5

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 N=[0m
 V=[01;32m
 
-VERSION=0.0.4
+VERSION=0.0.5
 NAME=huayra-curriculum
 
 all:
--- app/components/huayra-version.js
+++ app/components/huayra-version.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  version: "0.0.4",
+  version: "0.0.5",
   mostrarDialog: false,
 });

456f674d267aba14656af7cc31d5edeb3526b8ec
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-01-07 16:01:11 -0300

    Arreglos menores en la hoja de estilo.

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import('app/styles/huayra-pasos.css');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.

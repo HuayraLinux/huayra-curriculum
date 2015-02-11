487b203f76f386324a8842f350ce45c4891855ec
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-11 20:11:18 -0300

    aplicando dialogos modales a estudios cursados.

--- app/controllers/asistente/paso3.js
+++ app/controllers/asistente/paso3.js
@@ -1,19 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  mingreso: "20",
+  megreso: "20",
+  mdescripcion: "una descripcion",
+
   actions: {
-    CrearEstudio: function() {
+    eliminar: function(estudio) {
+      estudio.destroyRecord();
+      this.get('model').save();
+    },
+
+    /* Destinadas al modal */
+    submitForm: function() {
+
       var estudios = this.get('model.estudios');
       var estudio = this.store.createRecord('estudio', {
+        ingreso: this.get('mingreso'),
+        egreso: this.get('megreso'),
+        descripcion: this.get('mdescripcion'),
       });
 
       estudios.pushObject(estudio);
       estudio.save();
       this.get('model').save();
+
+      this.set('mingreso', '');
+      this.set('megreso', '');
+      this.set('mdescripcion', '');
     },
-    eliminar: function(estudio) {
-      estudio.destroyRecord();
-      this.get('model').save();
+
+    restoreModel: function() {
     }
   }
 });

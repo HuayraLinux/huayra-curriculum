f006f256db124185cf7e6e64ef9d5df6ee528448
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-08 20:42:22 -0300

    Fixeando tests
    Usaba algo de node en el entorno de PhantomJS para los tests, este commit debería fixearlo

--- app/initializers/set-wmclass.js
+++ app/initializers/set-wmclass.js
@@ -1,7 +1,11 @@
+import {inNWJS} from '../service';
+
 export function initialize(/* container, application */) {
   // application.inject('route', 'foo', 'service:foo');
-  /* Hack para setear WM_CLASS */
-  require('nwjs-hack').set_wmclass("huayra-curriculum", true);
+  if(inNWJS) {
+    /* Hack para setear WM_CLASS */
+    require('nwjs-hack').set_wmclass("huayra-curriculum", true);
+  }
 }
 
 export default {

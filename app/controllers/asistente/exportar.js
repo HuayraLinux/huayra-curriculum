298964b0e3d1afe5b885ec1dc71a119efdccf423
Ignacio Losiggio <iglosiggio@gmail.com>
2017-11-29 16:21:06 -0300

    Nueva plantilla y modificaciones para mejorar el exportador

--- app/controllers/asistente/exportar.js
+++ app/controllers/asistente/exportar.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import {service} from '../../service';
-
-
+import moment from 'moment';
 
 export default Ember.Controller.extend({
   guardando: false,
@@ -11,22 +10,25 @@ export default Ember.Controller.extend({
   actions: {
     guardarODT: function() {
       const ruta_destino = '/tmp/_temporal.docx';
-      const datos_template = this.get('model').serialize();
+      const datos_template = this.get('model').serialize({forExport: true});
       
       this.set('guardando', true);
       
       const estudios = this.get('model.estudios');
       if (estudios.get('length') > 0) {
-        datos_template.estudios = estudios.map(e => e.serialize());
+        datos_template.estudios = estudios.map(e => e.serialize({forExport: true}));
       }
 
       const experiencias = this.get('model.experiencias');
       if (experiencias.get('length') > 0) {
-        datos_template.experiencias = experiencias.map(e => e.serialize());
+        datos_template.experiencias = experiencias.map(e => e.serialize({forExport: true}));
       }
 
+      /* Insertar fecha del export */
+      datos_template.fechaactual = moment().format("DD/MM/YYYY");
+
       this.get('conversor')
-        .ejecutar('plantillas/cv.docx', datos_template, ruta_destino)
+        .ejecutar('plantillas/cv2.docx', datos_template, ruta_destino)
         .then((ruta_seleccionada) => {
           this.set('mensaje', "Se ha generado el archivo " + ruta_seleccionada);
           this.set('guardando', false);
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,2 +1,14 @@
 import { LSSerializer } from 'ember-localstorage-adapter';
-export default LSSerializer.extend();
+
+export default LSSerializer.extend({
+    serialize({record}, options) {
+        let json = this._super(...arguments);
+        if(options.forExport) {
+            record.eachAttribute(attribute =>
+                json[`hay${attribute}`] = !!record.get(attribute));
+            record.eachRelationship(relationship =>
+                json[`hay${relationship}`] = record.get(`${relationship}.length`) > 0);
+        }
+        return json;
+    }
+});

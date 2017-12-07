008429ee9b38951422450a8f0a676bb0b7a61099
Ignacio Losiggio <iglosiggio@gmail.com>
2017-12-07 15:10:14 -0300

    Agregar botón de "guardar y abrir"

--- app/controllers/asistente/exportar.js
+++ app/controllers/asistente/exportar.js
@@ -16,48 +16,58 @@ export default Ember.Controller.extend({
   mensaje: '',
   conversor: service('conversor'),
 
+  guardar() {
+    const ruta_destino = '/tmp/_temporal.docx';
+
+    return this.get('model').save().then(() => {
+      const datos_template = this.get('model').serialize({ forExport: true });
+
+      this.set('guardando', true);
+
+      const estudios = this.get('model.estudios');
+      const experiencias = this.get('model.experiencias');
+
+      /* Insertar fecha del export */
+      datos_template.fechaactual = moment().format("DD/MM/YYYY");
+
+      return Promise.all([datos_template, estudios, experiencias]);
+    }).then(([datos_template, estudios, experiencias]) => {
+      if (estudios.get('length') > 0) {
+        datos_template.estudios = estudios.map(e => e.serialize({ forExport: true }));
+      }
+
+      if (experiencias.get('length') > 0) {
+        datos_template.experiencias = experiencias.map(e => e.serialize({ forExport: true }));
+      }
+      return datos_template;
+    }).then(datos_template => {
+      return this.get('conversor').ejecutar(
+        this.get('plantilla.plantilla'),
+        datos_template,
+        ruta_destino
+      );
+    }).then((ruta_seleccionada) => {
+      this.set('mensaje', "Se ha generado el archivo " + ruta_seleccionada);
+      this.set('guardando', false);
+      return ruta_seleccionada;
+    }).catch((mensaje_error) => {
+      this.set('mensaje', mensaje_error);
+      this.set('guardando', false);
+      throw mensaje_error;
+    });
+  },
+
   actions: {
     seleccionarPlantilla(plantilla) {
       this.set('model.plantilla', plantilla.nombre);
     },
+    guardarYAbrir() {
+      const { execFile } = requireNode('child_process');
+      return this.guardar()
+                 .then(ruta => execFile('xdg-open', [ruta]));
+    },
     guardarODT() {
-      const ruta_destino = '/tmp/_temporal.docx';
-
-      return this.get('model').save().then(() => {
-        const datos_template = this.get('model').serialize({ forExport: true });
-
-        this.set('guardando', true);
-
-        const estudios = this.get('model.estudios');
-        const experiencias = this.get('model.experiencias');
-
-        /* Insertar fecha del export */
-        datos_template.fechaactual = moment().format("DD/MM/YYYY");
-
-        return Promise.all([datos_template, estudios, experiencias]);
-      }).then(([datos_template, estudios, experiencias]) => {
-        if (estudios.get('length') > 0) {
-          datos_template.estudios = estudios.map(e => e.serialize({ forExport: true }));
-        }
-
-        if (experiencias.get('length') > 0) {
-          datos_template.experiencias = experiencias.map(e => e.serialize({ forExport: true }));
-        }
-        return datos_template;
-      }).then(datos_template => {
-        return this.get('conversor').ejecutar(
-          this.get('plantilla.plantilla'),
-          datos_template,
-          ruta_destino
-        );
-      }).then((ruta_seleccionada) => {
-        this.set('mensaje', "Se ha generado el archivo " + ruta_seleccionada);
-        this.set('guardando', false);
-      }).catch((mensaje_error) => {
-        this.set('mensaje', mensaje_error);
-        this.set('guardando', false);
-        throw mensaje_error;
-      });
+      return this.guardar();
     }
   }
 });

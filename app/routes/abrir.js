ec5139c95e367687e42c93781e9c73f02febd1ee
Ignacio Losiggio <iglosiggio@gmail.com>
2017-12-07 12:16:21 -0300

    Limpiar un toque el código

--- app/routes/abrir.js
+++ app/routes/abrir.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   moment: Ember.inject.service(),
 
-  model: function() {
+  model() {
     this.get('moment').changeLocale('es');
     return this.store.findAll('curriculum');
   },

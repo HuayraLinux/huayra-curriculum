5bea43abed7b0579206fa1ea32b51628667b51cd
malefico <malefico3d@gmail.com>
2017-12-13 15:32:39 -0300

    Ajustes para mejorar legibilidad

--- app/data.js
+++ app/data.js
@@ -1,9 +1,9 @@
 export const pasos = [
-  { numero: 1, ruta: 'asistente.paso1', nombre: 'Datos Personales 1' },
-  { numero: 2, ruta: 'asistente.paso2', nombre: 'Datos Personales 2' },
+  { numero: 1, ruta: 'asistente.paso1', nombre: 'Datos 1' },
+  { numero: 2, ruta: 'asistente.paso2', nombre: 'Datos 2' },
   { numero: 3, ruta: 'asistente.paso3', nombre: 'Formación' },
-  { numero: 4, ruta: 'asistente.paso4', nombre: 'Experiencia laboral' },
-  { numero: 5, ruta: 'asistente.paso5', nombre: 'Datos complementarios' },
+  { numero: 4, ruta: 'asistente.paso4', nombre: 'Experiencia' },
+  { numero: 5, ruta: 'asistente.paso5', nombre: 'Otros' },
   { numero: 6, ruta: 'asistente.paso6', nombre: 'Objetivo personal' },
   { numero: 7, ruta: 'asistente.plantilla', nombre: 'Plantilla' },
   { numero: 8, ruta: 'asistente.exportar',  nombre: 'Finalizando...' },
